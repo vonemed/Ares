48d08f5f8beb4db9f0c94dadc8f58ecd8a300828
polezniy <glebadem@gmail.com>
2020-11-11 19:25:54 +0300

    Updating Component class

--- src/ares/Component.cpp
+++ src/ares/Component.cpp
@@ -1,1 +1,30 @@
 #include "Component.h"
+#include "Entity.h"
+
+namespace ares
+{
+	void Component::tick()
+	{
+		onTick();
+	}
+
+	void Component::render()
+	{
+		onRender();
+	}
+
+	void Component::onInitialize() { }
+	void Component::onTick() { }
+	void Component::onRender() { }
+
+
+	std::shared_ptr<Entity> Component::getEntity()
+	{
+		return entity.lock();
+	}
+
+	std::shared_ptr<Core> Component::getCore()
+	{
+		return getEntity()->getCore();
+	}
+}
--- src/ares/Component.h
+++ src/ares/Component.h
@@ -1,9 +1,31 @@
+#ifndef ARES_COMPONENT_H // If not defined by compiler
+#define ARES_COOMPONENT_H
+
 #include <memory>
 
 namespace ares
 {
+	struct Entity;
+	struct Core;
+
 	struct Component
 	{
-		int dummy;
+		friend struct ares::Entity;
+
+		virtual void onInitialize();
+
+		void tick();
+		virtual void onTick();
+
+		void render();
+		virtual void onRender();
+
+		std::shared_ptr<Entity> getEntity();
+		std::shared_ptr<Core> getCore();
+
+	private:
+		std::weak_ptr<Entity> entity;
 	};
 }
+
+#endif
