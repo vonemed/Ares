df65375874f2d9bb4e0d33593e1d53702a5ccdb8
polezniy <glebadem@gmail.com>
2020-11-11 14:01:20 +0300

    Updating Entity class

--- src/ares/Entity.cpp
+++ src/ares/Entity.cpp
@@ -1,1 +1,28 @@
 #include "Entity.h"
+#include "Component.h"
+
+namespace myengine
+{
+
+    void Entity::tick()
+    {
+        for (size_t ci = 0; ci < components.size(); ci++) // activating components
+        {
+            components.at(ci)->tick();
+        }
+    }
+
+    void Entity::render()
+    {
+        for (size_t ci = 0; ci < components.size(); ci++) // rendering components
+        {
+            components.at(ci)->render();
+        }
+    }
+
+    std::shared_ptr<Core> Entity::getCore() //
+    {
+        return core.lock(); // Returning a shared pointer
+    }
+
+}
--- src/ares/Entity.h
+++ src/ares/Entity.h
@@ -1,23 +1,54 @@
-#include <memory>
 #include <vector>
+#include <memory>
 
-namespace ares
+namespace myengine
 {
-	struct Component;
-	struct Entity
-	{
-		template <typename T>
-		std::shared_ptr<T> addComponent()
-		{
-			std::shared_ptr<T >> rtn = std::make_shared<T>();
 
-			components.push_back(rtn);
+    struct Component;
+    struct Core;
 
-			return rtn;
-		}
+    struct Entity
+    {
+        friend struct ::myengine::Core;
 
+        template <typename T>
+        std::shared_ptr<T> addComponent()
+        {
+            std::shared_ptr<T> rtn = std::make_shared<T>();
+            rtn->entity = self;
 
-	private:
-		std::vector<std::shared_ptr<Component>> components;
-	};
+            components.push_back(rtn);
+
+            rtn->onInitialize();
+
+            return rtn;
+        }
+
+        template <typename T> // Template function
+        std::shared_ptr<T> getComponent()
+        {
+            for (size_t i = 0; i < components.size(); i++)
+            {
+                std::shared_ptr<T> rtn = std::dynamic_pointer_cast<T>(components(i)); // Trying to convert every other component to the requested one
+
+                if (!rtn) continue; // If rtn equals NULL - then continue iteration
+
+                return rtn; // Once found it - return it
+            }
+
+            throw std::exception "Error: couldn't find the rquested type";
+        }
+
+        void tick();
+        void render();
+
+        std::shared_ptr<Core> getCore();
+
+    private:
+        std::vector<std::shared_ptr<Component>> components;
+        std::weak_ptr<Core> core;
+        std::weak_ptr<Entity> self;
+
+    };
+
 }
