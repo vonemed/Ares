ac42edba30ab0187c40db07d03b2234d51374532
polezniy <glebadem@gmail.com>
2020-11-12 17:33:42 +0300

    Initial upload of core classes

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -5,12 +5,22 @@ using namespace ares;
 
 int main()
 {
+	// Initialization of engine
 	std::shared_ptr<Core> core = Core::initialize();
 
+	// Creating a single in-game object
 	std::shared_ptr<Entity> pe = core->addEntity();
-	//std::shared_ptr<Component> pc = pe->addComponent<Component>();
 
-	core->initialize();
+	// Add a component 
+	std::shared_ptr<Component> pc = pe->addComponent<Component>();
+
+	core->Start();
+
+	const GLfloat positions[] = {
+		0.0f, 0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f
+	};
 
 	return 0;
 }
